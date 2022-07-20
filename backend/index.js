a60bcd068fc541555685b81dab0de4dad6836ff9
rendisagala <rendisagala19@gmail.com>
2022-07-20 14:31:21 +0700

    install packages backend

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express();
+
+// mongodb
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log(`Database is connected..`));
+
+// cors
+app.use(cors());
+
+//format
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cookie-parser
+app.use(cookieParser());
+
+// routes
+
+// port
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server is running....`));
