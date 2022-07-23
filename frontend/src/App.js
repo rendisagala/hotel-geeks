8a78e1da9b06811158563dad0afe1eb221c58e44
rendisagala <rendisagala19@gmail.com>
2022-07-23 22:29:55 +0700

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="object-contain lg:px-64 overflow-auto h-full flex flex-col">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>{" "}
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import NavigationBar from "../components/NavigationBar";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <NavigationBar />
+      </div>
+    );
+  }
+}
+
+export default Home;
