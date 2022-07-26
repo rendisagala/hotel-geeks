d7eacaa5156b76a45b49416470e5c4be2424d454
rendisagala <rendisagala19@gmail.com>
2022-07-26 14:21:08 +0700

    API

--- frontend/src/assets/image.js
+++
@@ -1,8 +0,0 @@
-import twostarhotel from "./image/twostarhotel.jpg";
-import threestarhotel from "./image/threestarhotel.jpg";
-import fourstarhotel from "./image/fourstarhotel.jpg";
-import fivestarhotel from "./image/fivestarhotel.jpg";
-
-const images = { twostarhotel, threestarhotel, fourstarhotel, fivestarhotel };
-
-export default images;
